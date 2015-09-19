1def5295aa964d203e97e33cb00a785ac3e669c3
Anna Ruokonen <anna.ruokonen@gmail.com>
2015-09-19 17:26:32 +0800

    update

--- js/order.js
+++ js/order.js
@@ -7,178 +7,60 @@ $(document).ready(function(){
   $('#order').submit(function(e){
     e.preventDefault();
 
+    var date_ = 0;
+    var huolto_ = "huolto";
+
+
+    var  radiosPerus = document.getElementsByName('perus');
+      for (var i = 0, length = radiosPerus.length; i < length; i++) {
+        if (radiosPerus[i].checked) {
+          // do whatever you want with the checked radio
+          huolto_ = radiosPerus[i].value;
+
+          // only one radio can be logically checked, don't check the rest
+          break;
+        }
+      }
+
+      var  radiosLaaja = document.getElementsByName('laaja');
+        for (var i = 0, length = radiosLaaja.length; i < length; i++) {
+          if (radiosLaaja[i].checked) {
+            // do whatever you want with the checked radio
+            huolto_ = radiosLaaja[i].value;
+
+            // only one radio can be logically checked, don't check the rest
+            break;
+          }
+        }
+
+
     // Grab the elements from the form to make up
     // an object containing name, email and message
-    var radios = document.getElementsByName('arvosana');
-    var palvelualttius_ = 0;
-    var tavoitettavuus_ = 0;
-    var huoltoarvosana_ = 0;
-    var lopputulos_ = 0;
-    var viimeistely_ = 0;
-    var tasmallisyys_ = 0;
-    var ystavallisyys_ = 0;
-    var siisteys_ = 0;
-    var ammattitaito_ = 0;
-    var tiedonkulku_ = 0;
-    var hintalaatu_ = 0;
-    var arvosana_ = 0;
-
-
-
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        arvosana_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-  var  radios1 = document.getElementsByName('palvelualttius');
-    for (var i = 0, length = radios1.length; i < length; i++) {
-      if (radios1[i].checked) {
-        // do whatever you want with the checked radio
-        palvelualttius_ = radios1[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-
-    var radios2 = document.getElementsByName('tavoitettavuus');
-    for (var i = 0, length = radios2.length; i < length; i++) {
-      if (radios2[i].checked) {
-        // do whatever you want with the checked radio
-        tavoitettavuus_ = radios2[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('huoltoarvosana');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        huoltoarvosana_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('lopputulos');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        lopputulos_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('viimeistely');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        viimeistely_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('tasmallisyys');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        tasmallisyys_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('ystavallisyys');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        ystavallisyys_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('siisteys');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        siisteys_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('ammattitaito');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        ammattitaito_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('tiedonkulku');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        tiedonkulku_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-    radios = document.getElementsByName('hintalaatu');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        hintalaatu_ = radios[i].value;
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-      }
-    }
-
-
     var data = {
       name: document.getElementById('name').value,
       email: document.getElementById('email').value,
       phone: document.getElementById('phone').value,
-      message: document.getElementById('message').value,
-      arvosana: arvosana_,
-      palvelualttius: palvelualttius_,
-      tavoitettavuus: tavoitettavuus_,
-      huoltoarvosana:  huoltoarvosana_,
-      lopputulos: lopputulos_,
-      viimeistely:  viimeistely_,
-      tasmallisyys: tasmallisyys_,
-      siisteys: siisteys_,
-      ammattitaito: ammattitaito_,
-      tiedonkulku: tiedonkulku_,
-      hintalaatu: hintalaatu_
-    }
+      postaddress: document.getElementById('postaddress').value,
+      address: document.getElementById('address').value,
+      date: document.getElementById('date').value,
+      huolto: huolto_
 
+    }
     this.reset();
 
-    // Run our Parse Cloud Code and
-    // pass our 'data' object to it
-    Parse.Cloud.run("feedback", data, {
+    // Run Parse Cloud Code
+    Parse.Cloud.run("order", data, {
       success: function(object) {
-        $('#response').html('Palaute lahetetty!').addClass('success').fadeIn('fast');
+        $('#response').html('Tilaus lähetetty!').addClass('success').fadeIn('fast');
       },
 
       error: function(object, error) {
         console.log(error);
-        $('#response').html('Virhe! Palautetta ei lahetetty!').addClass('error').fadeIn('fast');
+        $('#response').html('Virhe! Tilausta ei lähetetty!').addClass('error').fadeIn('fast');
       }
     });
+
   });
+
 
 });
